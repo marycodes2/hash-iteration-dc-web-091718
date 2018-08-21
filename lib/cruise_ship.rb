41c7438778b7ffdc6342a1efd7d85b6a67802bcb
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-21 14:36:06 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,6 +11,7 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == ":suite_a" && name.starts_with?("A")
       winner = name 
+      print winner
     end
   end
 winner 
